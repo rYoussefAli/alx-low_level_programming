14b1664e5f1d30216edc95fdd9979d233afd8954
Youssef Ali <aliyoussef200399@gmail.com>
2023-02-18 17:23:04 +0200

    add

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,6 +4,15 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int n;
