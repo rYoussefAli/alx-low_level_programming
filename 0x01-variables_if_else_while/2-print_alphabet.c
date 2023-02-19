97c603d83eadeee659ba737f483c800524399f0f
Youssef Ali <aliyoussef200399@gmail.com>
2023-02-19 18:48:05 +0200

    add: new file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -14,9 +14,9 @@
 
 int main(void)
 {
-    char ch;
-    for (ch = 'a'; ch <= 'z'; ch++)
-        putchar(ch);
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
 
-    return (0);
+	return (0);
 }
