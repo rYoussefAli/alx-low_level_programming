472f5a0a1377629deffefe612d70971efc3ec776
Youssef Ali <aliyoussef200399@gmail.com>
2023-02-20 14:02:03 +0200

    add

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: Here should be a description
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
