38493a951f7d94d3a175c6c70324cb514aa114c4
Youssef Ali <aliyoussef200399@gmail.com>
2023-02-20 16:03:20 +0200

    add

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,7 +19,7 @@ int main(void)
 	for (ch1 = '0'; ch1 <= '9'; ch1++)
 		for (ch2 = '0'; ch2 < '9'; ch2++)
 			for (ch3 = ch1; ch3 <= '9'; ch3++)
-				for (ch4 = ch2+1; ch4 <= '9'; ch4++)
+				for (ch4 = ch2 + 1; ch4 <= '9'; ch4++)
 				{
 					putchar(ch1);
 					putchar(ch2);
