9e3bdac9400f6ace3cc7838c7a152e4a6762656d
Youssef Ali <aliyoussef200399@gmail.com>
2023-07-06 22:07:15 -0400

    edit: functions

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+# include "main.h"
 /**
  * print_sign - checks the parity of the number
  * @n: the parameter
@@ -8,16 +9,16 @@
 int print_sign(int n)
 {
 	if (!n) {
-		putchar('0');
+		_putchar('0');
 		return 0;
 	}
 	else {
 		if (n > 0) {
-			putchar('+');
+			_putchar('+');
 			return 1;
 		}
 		else {
-			putchar ('-');
+			_putchar ('-');
 			return -1;
 		}
 	}
