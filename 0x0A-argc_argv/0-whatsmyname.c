7d1e4272f8375184c6e355d58a27baa012b57fc0
Youssef Ali <aliyoussef200399@gmail.com>
2023-07-09 13:00:55 -0400

    add: new file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - main
+ * @argc: the number of arguments
+ * @argv: the argument vector
+ *
+ * Return: print the file name.
+ */
+int main(int argc, char *argv[])
+{
+	char *i = argv[0];
+	
+	while(*i != '\0')
+	{
+		_putchar(*i);
+		i++;
+	}
+	return (0);
+}
