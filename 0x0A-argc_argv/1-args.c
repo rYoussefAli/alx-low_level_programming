475ebfea44165848de820ba95034b206b5c438ba
Youssef Ali <aliyoussef200399@gmail.com>
2023-07-09 13:56:36 -0400

    edits

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,4 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -10,7 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-	printf("%d\n", argc-1);
+	printf("%d\n", argc - 1);
 
 	(void)argv;
 
