5b1c4b8721c87646c6bda9b1913c2416d02047ab
Youssef Ali <aliyoussef200399@gmail.com>
2023-07-13 15:08:23 -0400

    add: files

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - _strdup
+ * @str: whole string
+ * Return: striing duplicated by malloc (free required after use)
+**/
+char *_strdup(char *str)
+{
+	char *v = NULL;
+	int n = 0;
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (*(str++))
+	{
+		n++;
+	}
+
+	str -= n + 1;
+
+	v = (char *)malloc(sizeof(char) * n);
+
+	if (!v)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		v[i] = str[i];
+	}
+
+	return (v);
+
+}
