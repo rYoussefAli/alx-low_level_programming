74382f03537af542d68d83fadc090f05dcec4939
Youssef Ali <aliyoussef200399@gmail.com>
2023-07-24 01:41:08 -0400

    edit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,11 +3,32 @@
 #include "main.h"
 
 /**
- * malloc_checked - malloc_checked
- * @b: size
- * Return: address to memory (free required after use)
+ * _calloc - _calloc
+ * @nmemb: number of bytes of type
+ * @size: size of type
+ * Return: address to memory filled with 0s (free required after use)
 **/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    void 
+	void *m = NULL;
+	unsigned int i;
+
+	if (!(nmeb && size))
+	{
+		return (NULL);
+	}
+
+	m = malloc(nmemb * size);
+
+	if (m == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb * size; i++)
+	{
+		m[i] = 0;
+	}
+
+	return (m);
 }
