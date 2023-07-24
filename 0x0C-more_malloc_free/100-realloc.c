cde4582600605faa852f30401588096a6ff7fe10
Youssef Ali <aliyoussef200399@gmail.com>
2023-07-24 14:11:28 -0400

    add: file-100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -36,7 +36,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	mv = (char *)v;
 	mptr = (char *)ptr;
 
-	for (i = 0; i < old_size; i++)
+	for (i = 0; i < (old_size < new_size ? old_size : new_size); i++)
 	{
 		mv[i] = mptr[i];
 	}
