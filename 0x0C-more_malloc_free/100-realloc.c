6611d4879870171056d18b119972710877e69a7f
Youssef Ali <aliyoussef200399@gmail.com>
2023-07-24 14:27:46 -0400

    edit: file-100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,7 +22,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == 0)
 	{
-		free(ptr);
+		if (ptr)
+		{
+			free(ptr);
+		}
 		return (NULL);
 	}
 
@@ -41,6 +44,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		{
 			mv[i] = mptr[i];
 		}
+
+		free(ptr);
 	}
 
 	return (v);
