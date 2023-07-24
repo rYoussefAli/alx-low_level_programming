a647bfb52001bf54bf93f3cf27256cbb6ad6baeb
Youssef Ali <aliyoussef200399@gmail.com>
2023-07-24 14:09:01 -0400

    add: file-100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * array_range - array_range
+ * @ptr: the pointer
+ * @old_size: old size of pointer
+ * @new_size: new size of pointer
+ * Return: different size of pointer (free required after use)
+**/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *v = NULL;
+	char *mv = NULL, *mptr = NULL;
+	unsigned int i;
+
+	if (old_size == new_size)
+	{
+		return (ptr);
+	}
+
+	if (!new_size)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	v = malloc(new_size);
+
+	if (!v)
+	{
+		return (NULL);
+	}
+
+	mv = (char *)v;
+	mptr = (char *)ptr;
+
+	for (i = 0; i < old_size; i++)
+	{
+		mv[i] = mptr[i];
+	}
+
+	return (v);
+}
--- 0x0C-more_malloc_free/main.c
+++ 0x0C-more_malloc_free/main.c
@@ -10,38 +10,45 @@
  *
  * Return: Nothing.
  */
-void simple_print_buffer(int *buffer, unsigned int size)
+void simple_print_buffer(char *buffer, unsigned int size)
 {
-    unsigned int i;
+	unsigned int i;
 
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
 }
 
 /**
- * main - check the code
+ * main - check the code for
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int *a;
+	char *p;
+	int i;
 
-    a = array_range(0, 10);
-    simple_print_buffer(a, 11);
-    free(a);
-    return (0);
+	p = malloc(sizeof(char) * 10);
+	p = _realloc(p, sizeof(char) * 10, sizeof(char) * 98);
+	i = 0;
+	while (i < 98)
+	{
+		p[i++] = 98;
+	}
+	simple_print_buffer(p, 98);
+	free(p);
+	return (0);
 }
