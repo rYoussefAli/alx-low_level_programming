e22a48376e46eb9317ef9f9ef796206c3e4375ec
Youssef Ali <aliyoussef200399@gmail.com>
2023-07-24 14:19:16 -0400

    add: file-100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * array_range - array_range
+ * _realloc - _realloc
  * @ptr: the pointer
  * @old_size: old size of pointer
  * @new_size: new size of pointer
@@ -20,7 +20,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	}
 
-	if (!new_size)
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
